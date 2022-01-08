42a6e2a957807eadbabf4622adbe7f7e79a19765
Abhijeet raut <Abhijeetraut456@gmail.com>
2022-01-08 16:15:28 +0530

    Deploy

---
+++ script.js
@@ -0,0 +1,87 @@
+const addUserBtn = document.getElementById('add-user');
+const doubleBtn = document.getElementById('double');
+const showMillionairesBtn = document.getElementById('show-millionaires');
+const sortBtn = document.getElementById('sort');
+const calculateWealthBtn = document.getElementById('calculate-wealth');
+const main = document.getElementById('main');
+
+let data = [];
+
+//Get Random user
+const getRandomUser = async () => {
+  const res = await fetch(`https://randomuser.me/api`);
+  const data = await res.json();
+  const user = data.results[0];
+  const newUser = {
+    name: `${user.name.first} ${user.name.last}`,
+    money: Math.floor(Math.random() * 1000000),
+  };
+
+  addData(newUser);
+};
+
+getRandomUser();
+getRandomUser();
+getRandomUser();
+//Double money
+const doubleMoney = () => {
+  data = data.map((user) => {
+    return { ...user, money: user.money * 2 };
+  });
+  updateDOM();
+};
+
+//show millionaires
+const showMillionaires = () => {
+  data = data.filter((user) => user.money > 1000000);
+  updateDOM();
+};
+
+//sort by richest
+const sortByRichest = () => {
+  data.sort((a, b) => {
+    return b.money - a.money;
+  });
+  updateDOM();
+};
+
+//calculate total wealth
+const calculateWealth = () => {
+  const wealth = data.reduce((acc, item) => (acc, item.money), 0);
+  const wealthEl = document.createElement('div');
+  wealthEl.innerHTML = `<h3><strong>Total Wealth</strong>${formatMoney(
+    wealth
+  )}</h3>`;
+  main.appendChild(wealthEl);
+};
+
+const addData = (obj) => {
+  data.push(obj);
+  updateDOM();
+};
+
+const updateDOM = (providedData = data) => {
+  //clear main div
+  main.innerHTML = '<h2><strong>Person</strong>Wealth</h2>';
+
+  providedData.forEach((item) => {
+    const element = document.createElement('div');
+    element.classList.add('person');
+    element.innerHTML = `<strong>${item.name}</strong>${formatMoney(
+      item.money
+    )}`;
+    main.appendChild(element);
+  });
+};
+
+//Formate money
+const formatMoney = (number) => {
+  return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+};
+
+//Event listener
+addUserBtn.addEventListener('click', getRandomUser);
+doubleBtn.addEventListener('click', doubleMoney);
+showMillionairesBtn.addEventListener('click', showMillionaires);
+sortBtn.addEventListener('click', sortByRichest);
+calculateWealthBtn.addEventListener('click', calculateWealth);
